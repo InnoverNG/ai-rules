9ea6a60ee2d1e4e440e9ede989c04a47e39558f4
Tochi <okorie.tochi@gmail.comgit config --global user.name Tochi>
2026-02-26 17:41:41 +0100

    made modification to cursor

--- src/AiRulesPlugin.php
+++ src/AiRulesPlugin.php
@@ -8,30 +8,24 @@ use Composer\Composer;
 use Composer\EventDispatcher\EventSubscriberInterface;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
-use Composer\DependencyResolver\Operation\InstallOperation;
-use Composer\DependencyResolver\Operation\UpdateOperation;
-use Composer\Installer\PackageEvent;
-use Composer\Installer\PackageEvents;
+use Composer\Script\Event;
+use Composer\Script\ScriptEvents;
 
 class AiRulesPlugin implements PluginInterface, EventSubscriberInterface
 {
-    private Composer $composer;
-    private IOInterface $io;
     private Installer $installer;
 
     public function activate(Composer $composer, IOInterface $io): void
     {
-        $this->composer = $composer;
-        $this->io = $io;
         $this->installer = new Installer($composer, $io);
     }
 
-    public function deactivate(Composer $composer, IOInterface $io): void
+    public function deactivate(Composer $_composer, IOInterface $_io): void
     {
         // Nothing to deactivate
     }
 
-    public function uninstall(Composer $composer, IOInterface $io): void
+    public function uninstall(Composer $_composer, IOInterface $_io): void
     {
         // We intentionally do not delete user files on uninstall
     }
@@ -39,38 +33,18 @@ class AiRulesPlugin implements PluginInterface, EventSubscriberInterface
     public static function getSubscribedEvents(): array
     {
         return [
-            PackageEvents::POST_PACKAGE_INSTALL => [['onPackageInstall', 0]],
-            PackageEvents::POST_PACKAGE_UPDATE   => [['onPackageUpdate', 0]],
+            ScriptEvents::POST_INSTALL_CMD => [['onPostInstall', 0]],
+            ScriptEvents::POST_UPDATE_CMD  => [['onPostUpdate', 0]],
         ];
     }
 
-    public function onPackageInstall(PackageEvent $event): void
+    public function onPostInstall(Event $event): void
     {
-        $operation = $event->getOperation();
-
-        if (!$operation instanceof InstallOperation) {
-            return;
-        }
-
-        if ($operation->getPackage()->getName() !== 'innoverng/ai-rules') {
-            return;
-        }
-
         $this->installer->copyStubs(install: true);
     }
 
-    public function onPackageUpdate(PackageEvent $event): void
+    public function onPostUpdate(Event $event): void
     {
-        $operation = $event->getOperation();
-
-        if (!$operation instanceof UpdateOperation) {
-            return;
-        }
-
-        if ($operation->getTargetPackage()->getName() !== 'innoverng/ai-rules') {
-            return;
-        }
-
         $this->installer->copyStubs(install: false);
     }
 }
